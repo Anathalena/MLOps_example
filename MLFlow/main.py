3bdc2775486b5c6e5a4926bdb286b4b3867b9a3e
mmakevic <milica.makevic@comtrade.com>
2023-05-24 12:52:13 +0200

    Add conv and pooling layers to architecture

--- MLFlow/main.py
+++ MLFlow/main.py
@@ -5,10 +5,11 @@ from dataloader import Data
 
 if __name__ == '__main__':
     mlflow.pytorch.autolog()
-    model = Model(input_size=28*28, hidden_size=100, num_classes=10, learning_rate=1e-3, batch_size=32)
+    model = Model(num_classes=10, learning_rate=1e-3, batch_size=32)
     data = Data(batch_size=32)
 
     trainer = pl.Trainer(max_epochs=5)
     trainer.fit(model=model, datamodule=data)
-    trainer.test(datamodule=data)
     trainer.validate(datamodule=data)
+    trainer.test(datamodule=data)
+    
--- MLFlow/model.py
+++ MLFlow/model.py
@@ -1,22 +1,29 @@
 import torch
 from torch import nn
 import lightning.pytorch as pl
-
+from torchmetrics import Accuracy
 
 
 class Model(pl.LightningModule):
-    def __init__(self, input_size, hidden_size, num_classes, learning_rate, batch_size):
+    def __init__(self, num_classes, learning_rate, batch_size):
         super().__init__()
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=num_classes)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=num_classes)
         self.lr = learning_rate
         self.batch_size = batch_size
         self.loss = nn.CrossEntropyLoss()
-        self.l1 = nn.Sequential(nn.Linear(input_size, hidden_size),
-                                nn.ReLU(),
-                                nn.Linear(hidden_size, num_classes))
+        self.l1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=24, kernel_size=5, stride=1, padding=0),
+                                nn.MaxPool2d(2,2),
+                                nn.Conv2d(in_channels=24, out_channels=48, kernel_size=5, stride=1, padding=0),
+                                nn.MaxPool2d(2,2))
+        self.l2 = nn.Sequential(nn.Linear(in_features=48*4*4, out_features=256),
+                                nn.ReLU(inplace=True),
+                                nn.Linear(in_features=256, out_features=num_classes))
         
     def forward(self, x):
-        x = x.view(-1,28*28)
-        output = self.l1(x)
+        x = self.l1(x)
+        x = x.view(-1,48*4*4)
+        output = self.l2(x)
         return output
     
     def configure_optimizers(self):
@@ -26,18 +33,21 @@ class Model(pl.LightningModule):
         x, y = train_batch
         out = self.forward(x)
         train_loss = self.loss(out,y)
-        self.log("train_loss", train_loss, on_epoch=True)
+        self.train_accuracy(out,y)
+        self.log("train_loss", train_loss, on_epoch=True, on_step=False)
         return train_loss
     
     def validation_step(self, batch, batch_idx):
         x, y = batch
         out = self.forward(x)
         val_loss = self.loss(out,y)
-        self.log("val_loss", val_loss, on_epoch=True)
+        self.val_accuracy(out,y)
+        self.log("val_acc", self.val_accuracy, on_epoch=True, on_step=False)
+        self.log("val_loss", val_loss, on_epoch=True, on_step=False)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         out = self.forward(x)
-        test_loss = self.loss(out,y)
-        self.log("test_loss", test_loss, on_epoch=True)
+        self.test_accuracy(out,y)
+        self.log("test_acc", self.test_accuracy, on_epoch=True, on_step=False)
 
