fc85920e9aa77c16d98b3844ac507a49248e9a35
irinam <irinam@comtrade.com>
2023-05-30 03:23:49 -0400

    Revert "Removed third FC layer"
    This reverts commit 9c7873c92f5e99fe9ce3c24e4dd2a426559d58b1.

--- MLFlow/main.py
+++ MLFlow/main.py
@@ -9,21 +9,19 @@ if __name__ == '__main__':
     remote_server_ui = 'http://127.0.0.1:5000'
     run_name="MNIST classification - testing"
     mlflow.set_tracking_uri(remote_server_ui)
-    mlflow.set_experiment("Test with no auto log")
+    mlflow.set_experiment("Test with auto log")
 
-    for lr in [0.001, 0.005, 0.01, 0.1]:
-        with mlflow.start_run(run_name=run_name) as run:
-        
-            model = Model(num_classes=10, learning_rate=lr, batch_size=32)
-            data = Data(batch_size=32)
+    mlflow.set_experiment("Test")
+    mlflow.pytorch.autolog()
+    mlflow.start_run(run_name="MNIST-classification")
 
-            mlflow.pytorch.log_model(model, "cnn-model")
-            mlflow.log_param("learning_rate", lr)
+    model = Model(num_classes=10, learning_rate=1e-3, batch_size=32)
+    data = Data(batch_size=32)
 
-            trainer = pl.Trainer(max_epochs=5)
-            trainer.fit(model=model, datamodule=data)
-            trainer.validate(datamodule=data)
-            trainer.test(datamodule=data)
+    mlflow.pytorch.log_model(model, "cnn-model")
 
-            print("Run with id {} and experiment id {} is done.".format(run.info.run_uuid, run.info.experiment_id))
+    trainer = pl.Trainer(max_epochs=5)
+    trainer.fit(model=model, datamodule=data)
+    trainer.validate(datamodule=data)
+    trainer.test(datamodule=data)
     
